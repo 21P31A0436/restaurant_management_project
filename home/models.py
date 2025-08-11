361ce5d0c8aa79d7f946749d1ba49ed49e5d0f62
Maddula Venkata Surendra kumar <99795947+21P31A0436@users.noreply.github.com>
2025-08-11 10:59:22 +0530

    27879

--- home/models.py
+++ home/models.py
@@ -3,3 +3,5 @@ from django.db import models
 # Create your models here.
 class Restaurant(models.Models):
     name = models.CharField(max_length=150)
+    def __str__(self):
+        return self.name
