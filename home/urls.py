ae209017966587329fafa19c26f5b8c34306dc72
Maddula Venkata Surendra kumar <99795947+21P31A0436@users.noreply.github.com>
2025-08-11 20:33:29 +0530

    27880

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('restaurant/', RestaurantNameView.as_view(), name='restaurant-name'),
+    # path('restaurant/', RestaurantNameView.as_view(), name='restaurant-name'),
+    path('restaurant-info/', RestaurantInfoView.as_view(), name='restaurant-info'),
+    
 ]
