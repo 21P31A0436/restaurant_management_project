273e449af094652c919498427b938b664c3a1c12
Maddula Venkata Surendra kumar <99795947+21P31A0436@users.noreply.github.com>
2025-08-11 11:18:58 +0530

    27879

--- home/views.py
+++ home/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.conf import settings
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Restaurant
+from .serializers import RestaurantSerializer
 # Create your views here.
+class RestaurantNameView(APIView):
+    def get(self, request):
+        restaurant = Restaurant.objects.first()
+        if restaurant:
+            serializer = RestaurantSerializer(restaurant)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response({"name": settings.RESTAURANT_NAME}, status=status.HTTP_200_OK)
+
