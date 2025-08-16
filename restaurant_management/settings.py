54cf3f48f900c6083b43486d7927b21c43425243
Maddula Venkata Surendra kumar <99795947+21P31A0436@users.noreply.github.com>
2025-08-16 20:06:19 +0530

    33671

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'django-insecure-s^x_u!itekxd=@a3o3zv4d%hw$j6d#2v358wgltxl8rc(-^t&p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+RESTAURANT_PHONE = "+91 1234567890"
 ALLOWED_HOSTS = ['*']
 
 
