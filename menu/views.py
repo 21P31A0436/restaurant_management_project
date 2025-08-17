4d989064cddfcead3832690d64939778616c517f
Maddula Venkata Surendra kumar <99795947+21P31A0436@users.noreply.github.com>
2025-08-17 19:48:42 +0530

    33668

---
+++ menu/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+class MenuItemView(APIView):
+    def get(self, request):
+        menu_items =[
+            {"id":1, "name": "veg Biryani", "price": 150},
+            {"id":2, "name":"paneer Butter Masala", "price":180}
+            {"id"3, "name":"Chicken", "price": 200}
+        ]
+        return Response(menu_items, status=status.HTTP_200_OK)
