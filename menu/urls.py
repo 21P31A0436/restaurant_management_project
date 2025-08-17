cb356f25196feb15bbb443a98acadfbf1e9e61e2
Maddula Venkata Surendra kumar <99795947+21P31A0436@users.noreply.github.com>
2025-08-17 22:11:38 +0530

    33672

--- menu/urls.py
+++ menu/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import MenuItemView
 
 urlpatterns = [
+    path('', home, name="home"),
     path('menu-items/', MenuItemView.as_view(), name="menu-items")
     
 ]
