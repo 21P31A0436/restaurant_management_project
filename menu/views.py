3835d3bad1fafb3964c75a334d3664f2ac175ccb
Maddula Venkata Surendra kumar <99795947+21P31A0436@users.noreply.github.com>
2025-08-19 19:27:55 +0530

    34704

--- menu/views.py
+++ menu/views.py
@@ -4,13 +4,20 @@ from rest_framework.response import Response
 from rest_framework import status
 
 class MenuItemView(APIView):
+    
     def get(self, request):
-        menu_items =[
-            {"id":1, "name": "veg Biryani", "price": 150},
-            {"id":2, "name":"paneer Butter Masala", "price":180}
-            {"id"3, "name":"Chicken", "price": 200}
-        ]
-        return Response(menu_items, status=status.HTTP_200_OK)
+        try:
+            menu_items =[
+                {"id":1, "name": "veg Biryani", "price": 150},
+                {"id":2, "name":"paneer Butter Masala", "price":180}
+                {"id"3, "name":"Chicken", "price": 200}
+            ]
+            return Response(menu_items, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response(
+                {"error":"Something went wrong while fetching menu items","details":str(e)},
+                status=status.HTTP_500_INTERNAL_SERVER_ERROR
+            )
     def home(request):
         context = {
             "restaurant_name": "SpiceHub Restaurant"
@@ -19,3 +26,4 @@ class MenuItemView(APIView):
     # def django.shortcuts import render
     def reservations(request):
         return render(request, "reservations.html")
+    
